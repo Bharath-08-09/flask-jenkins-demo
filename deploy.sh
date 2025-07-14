0e2b6e3fc2b5b8d036d22ff84614147894b89eb4
Ubuntu <ubuntu@ip-10-0-0-11.ec2.internal>
2025-07-14 07:56:29 +0000

    Fix PEP 668 error using virtual environment

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+
 echo "Running Backend Setup..."
 
 sudo apt update -y
-sudo apt install python3-pip -y
-pip3 install -r requirements.txt
-
+sudo apt install python3.12-venv -y
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
 pkill -f "python3 app.py" || echo "No running Flask app found"
-
-nohup python3 app.py > app.log 2>&1 &
+nohup venv/bin/python app.py > app.log 2>&1 &
