aad68d0a6c7b83c146b57a25c2d227c7d753b5de
Ubuntu <ubuntu@ip-10-0-0-11.ec2.internal>
2025-07-14 08:00:53 +0000

    Use --break-system-packages to avoid venv

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,10 @@
 echo "Running Backend Setup..."
 
 sudo apt update -y
-sudo apt install python3.12-venv -y
-python3 -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
+sudo apt install python3-pip -y
+
+pip3 install --break-system-packages -r requirements.txt
+
 pkill -f "python3 app.py" || echo "No running Flask app found"
-nohup venv/bin/python app.py > app.log 2>&1 &
+
+nohup python3 app.py > app.log 2>&1 &
