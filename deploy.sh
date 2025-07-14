63c131d30a94ee91263707fa93e90f1708e94b16
Ubuntu <ubuntu@ip-10-0-0-11.ec2.internal>
2025-07-14 08:09:49 +0000

    Fix: Add Jenkins user PYTHONPATH for Flask

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,11 @@ sudo apt install python3-pip -y
 
 pip3 install --break-system-packages -r requirements.txt
 
+# Set Python path for Jenkins-installed packages
+export PATH=$PATH:/var/lib/jenkins/.local/bin
+export PYTHONPATH=$PYTHONPATH:/var/lib/jenkins/.local/lib/python3.12/site-packages
+
 pkill -f "python3 app.py" || echo "No running Flask app found"
 
+# Start Flask app and log output
 nohup python3 app.py > app.log 2>&1 &
